c9b2b4b4279fc4c817d9c993207b630b6f3a66d0
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-05-27 14:08:59 +0100

    save

--- play/hello-babel-template.mjs
+++ play/hello-babel-template.mjs
@@ -4,20 +4,26 @@ It allows you to write strings of code with placeholders
 that you can use instead of manually building up a massive AST.
 */
 import template from "babel-template";
-//const template = _template.default;
 import _generate from "@babel/generator";
 const generate = _generate.default;
 
 import * as _t from "babel-types";
 const t = _t.default;
 
+import compast from "compact-js-ast"
+
 const buildRequire = template(`
   var IMPORT_NAME = require(SOURCE);
 `);
 
+const identifier = t.identifier("myModule");
+const stringLiteral = t.stringLiteral("my-module");
+console.log(compast(identifier, { parse: false, }),
+  compast(stringLiteral, { parse: false, }));
+
 const ast = buildRequire({
-  IMPORT_NAME: t.identifier("myModule"),
-  SOURCE: t.stringLiteral("my-module")
+  IMPORT_NAME: identifier,
+  SOURCE: stringLiteral
 });
 
-console.log(generate(ast).code);
+console.log(generate(ast).code); // var myModule = require("my-module");
