c4ae8d9cfd8404c64a1dfd2f46e42c98663dc1e3
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-05-27 18:37:10 +0100

    save

--- play/get-sibling-path.mjs
+++ play/get-sibling-path.mjs
@@ -1,11 +1,6 @@
 import * as babylon from "babylon";
 import _traverse from "@babel/traverse";
 const traverse = _traverse.default;
-//console.log(traverse)
-import _generate from "@babel/generator";
-const generate = _generate.default;
-
-import compast from "compact-js-ast"
 
 const code = `
 var a = 1; // pathA, path.key = 0
@@ -35,20 +30,3 @@ traverse(ast, {
   }
 });
 
-/*
-console.log(compast(ast, {
-  parse: false,
-  hide: ["directives", "generator", "async"]
-}));
-
-const output = generate(
-  ast, { // See options https://babel.dev/docs/babel-generator#options
-    retainLines: false, // Attempt to use the same line numbers in the output code as in the source code (helps preserve stack traces)
-    compact: true, // Set to true to avoid adding whitespace for formatting
-    concise: true, // Set to true to avoid adding whitespace for formatting
-    quotes: "double",
-  },
-  code
-);
-console.log(output.code);
-*/
