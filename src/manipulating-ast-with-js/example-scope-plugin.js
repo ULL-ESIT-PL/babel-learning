558c7d1c968f92dba42b342177b3831c328c0096
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-06-03 19:18:31 +0100

    save

--- src/manipulating-ast-with-js/example-scope-plugin.js
+++ src/manipulating-ast-with-js/example-scope-plugin.js
@@ -3,16 +3,16 @@ module.exports = ({ types: t }) => {
     visitor: {
       CallExpression(path) {
         const { scope, node } = path;
-        if (node.callee.name === 'require'
-          && node.arguments.length === 1
-          && t.isStringLiteral(node.arguments[0])
-          && node.arguments[0].value === 'foo-bar'
-        ) {
+        if (!(node.callee.name === 'require'       && 
+              node.arguments.length === 1          && 
+              t.isStringLiteral(node.arguments[0]) && 
+              node.arguments[0].value === 'foo-bar')) return; 
           const localIdentifier = path.parent.id.name; // f
-          scope.bindings[localIdentifier].referencePaths.forEach(p => {
+          for (const p of scope.bindings[localIdentifier].referencePaths) { /* Same as scope.getBinding(localIdentifier) */
+            if (!(p.parentPath.isCallExpression()  && 
+                  p.parent.callee === p.node)) continue; 
             p.parentPath.replaceWith(t.NumericLiteral(99));
-          }); 
-        }
+          };   
       }
     }
   }
