94da5e214233770e2c2a9be089a4570d94002be8
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-06-13 10:52:16 +0100

    save

--- src/parser/error-example.cjs
+++ src/parser/error-example.cjs
@@ -4,4 +4,39 @@ const ast = parse(`a b`, { errorRecovery: true });
 
 console.log(ast.errors[0].code); // BABEL_PARSER_SYNTAX_ERROR
 console.log(ast.errors[0].reasonCode); // MissingSemicolon
-console.log(JSON.stringify(ast, null, 2));
+console.log(JSON.stringify(
+  ast, 
+  (key, value) => { return ['loc', 'start', 'end', 'directives', 'comments'].includes(key) ? undefined : value },
+  2)
+); /* Notice how some AST is still generated despite the syntax error:
+{
+  "type": "File",
+  "errors": [
+    {
+      "code": "BABEL_PARSER_SYNTAX_ERROR",
+      "reasonCode": "MissingSemicolon",
+      "pos": 1
+    }
+  ],
+  "program": {
+    "type": "Program",
+    "sourceType": "script",
+    "interpreter": null,
+    "body": [
+      {
+        "type": "ExpressionStatement",
+        "expression": {
+          "type": "Identifier",
+          "name": "a"
+        }
+      },
+      {
+        "type": "ExpressionStatement",
+        "expression": {
+          "type": "Identifier",
+          "name": "b"
+        }
+      }
+    ]
+  }
+*/
