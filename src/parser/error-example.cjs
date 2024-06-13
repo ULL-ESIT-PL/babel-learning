361d4a19199dd54e601982d0eaf80623565c2383
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-06-13 10:34:51 +0100

    save

--- src/parser/error-example.cjs
+++ src/parser/error-example.cjs
@@ -4,3 +4,4 @@ const ast = parse(`a b`, { errorRecovery: true });
 
 console.log(ast.errors[0].code); // BABEL_PARSER_SYNTAX_ERROR
 console.log(ast.errors[0].reasonCode); // MissingSemicolon
+console.log(JSON.stringify(ast, null, 2));
