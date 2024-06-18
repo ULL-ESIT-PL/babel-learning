29838a474c9b4b534590a5ac4c96c5fcf37fa670
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-06-18 23:31:19 +0100

    save

--- src/state/hello-state-plugin.cjs
+++ src/state/hello-state-plugin.cjs
@@ -5,19 +5,24 @@ state git:(main) ✗ pwd
 */
 module.exports = function (babel) {
   const { template, types: t } = babel;
-  let sneak = template(`console.log(COUNT)`);
+  let sneak = template(`console.log("Identifier '"+VAR + "' was used " + COUNT + " times")`);
 
   return {
-    name: "hello-state-plugin",
+    name: "hello-state-plugin", 
     visitor: {
       Identifier(path, state) {
         if (path.node.name === state.opts.targetIdentifier) {
-          state.identifierCount = (parseInt(state.identifierCount)+1) || 1;
+          state.identifierCount = state.identifierCount+1 || 1;
         }
       },
       Program: {
         exit(path, state) {
-          path.node.body.push(sneak({ COUNT: t.NumericLiteral(state.identifierCount || 0) }));
+          path.node.body.push(
+            sneak({ 
+              VAR: t.StringLiteral(state.opts.targetIdentifier),
+              COUNT: t.NumericLiteral(state.identifierCount || 0) 
+            })
+          );
           console.error(`Identifier "${state.opts.targetIdentifier}" was used ${state.identifierCount} times.`);
         }
       }
