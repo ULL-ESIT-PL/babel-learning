0ce2acf09e931d33130c65a5e298ab15cdec4ef6
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-06-19 11:39:14 +0100

    save

---
+++ src/state/hello-options-plugin.cjs
@@ -0,0 +1,10 @@
+module.exports = function({ types: t }) {
+  return {
+    visitor: {
+      FunctionDeclaration(path, state) {
+        console.log(state.opts);
+        // { option1: true, option2: false }
+      }
+    }
+  }
+}
