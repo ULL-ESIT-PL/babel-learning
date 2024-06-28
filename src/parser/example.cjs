8c76d73fadd4cf852129dfaf74c0579ef22cd9db
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-06-28 09:55:44 +0100

    save

--- src/parser/example.cjs
+++ src/parser/example.cjs
@@ -18,3 +18,93 @@ const skip = (key, value) => {
   return value;
 }
 console.log(JSON.stringify(ast, skip, 2));
+/* Execution:
+➜  babel-learning git:(main) node src/parser/example.cjs 
+{
+  "type": "File",
+  "errors": [],
+  "program": {
+    "type": "Program",
+    "sourceType": "module",
+    "interpreter": null,
+    "body": [
+      {
+        "type": "VariableDeclaration",
+        "declarations": [
+          {
+            "type": "VariableDeclarator",
+            "id": {
+              "type": "Identifier",
+              "name": "element",
+              "typeAnnotation": {
+                "type": "TypeAnnotation",
+                "typeAnnotation": {
+                  "type": "GenericTypeAnnotation",
+                  "typeParameters": null,
+                  "id": {
+                    "type": "QualifiedTypeIdentifier",
+                    "qualification": {
+                      "type": "Identifier",
+                      "name": "React"
+                    },
+                    "id": {
+                      "type": "Identifier",
+                      "name": "Element"
+                    }
+                  }
+                }
+              }
+            },
+            "init": {
+              "type": "JSXElement",
+              "openingElement": {
+                "type": "JSXOpeningElement",
+                "name": {
+                  "type": "JSXIdentifier",
+                  "name": "a"
+                },
+                "attributes": [
+                  {
+                    "type": "JSXAttribute",
+                    "name": {
+                      "type": "JSXIdentifier",
+                      "name": "href"
+                    },
+                    "value": {
+                      "type": "StringLiteral",
+                      "extra": {
+                        "rawValue": "https://www.google.com",
+                        "raw": "\"https://www.google.com\""
+                      },
+                      "value": "https://www.google.com"
+                    }
+                  }
+                ],
+                "selfClosing": false
+              },
+              "closingElement": {
+                "type": "JSXClosingElement",
+                "name": {
+                  "type": "JSXIdentifier",
+                  "name": "a"
+                }
+              },
+              "children": [
+                {
+                  "type": "JSXText",
+                  "extra": {
+                    "rawValue": "Hello, world!",
+                    "raw": "Hello, world!"
+                  },
+                  "value": "Hello, world!"
+                }
+              ]
+            }
+          }
+        ],
+        "kind": "const"
+      }
+    ]
+  }
+}
+*/
