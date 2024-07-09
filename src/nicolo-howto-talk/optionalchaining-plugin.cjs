b0a44e8767a36f0afe5df239a616b7bf21944f54
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-07-09 11:10:47 +0100

    s/propert/property/ bug

--- src/nicolo-howto-talk/optionalchaining-plugin.cjs
+++ src/nicolo-howto-talk/optionalchaining-plugin.cjs
@@ -8,7 +8,7 @@ module.exports = function myPlugin(babel, options) {
     },
     visitor: {
       OptionalMemberExpression(path) {
-        let { object, propert, computed} = path.node;
+        let { object, property, computed} = path.node;
         let memberExp = t.memberExpression(object, property, computed);
         let undef = path.scope.buildUndefinedNode();
         path.replaceWith(
