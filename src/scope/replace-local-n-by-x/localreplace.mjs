a70ceeac1d9c8959a7c6c7f80f85dc4a67f00b7d
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-04-01 15:10:31 +0100

    save

--- src/scope/replace-local-n-by-x/localreplace.mjs
+++ src/scope/replace-local-n-by-x/localreplace.mjs
@@ -1,12 +1,12 @@
-const varName = process.env["VARNAME"] || "z";
-const replace = process.env["REPLACE"] || "z";
+const varName = process.env["VARNAME"] || "z"; // Get the variable name from the environment variable VARNAME, default to "z"
+const replace = process.env["REPLACE"] || "z"; // Get the replacement name from the environment variable REPLACE, default to "z"
 
 export default function({ types: t }) {
   return {
     visitor: {
       FunctionDeclaration(path) {
-        if (path.scope.hasOwnBinding(varName)) {
-          path.traverse({
+        if (path.scope.hasOwnBinding(varName)) { // Check if the function has a binding for the variable name
+          path.traverse({ // Traverse the function body
             Identifier(path) {
               if (path.node.name ===  varName) {
                 path.node.name = replace;
